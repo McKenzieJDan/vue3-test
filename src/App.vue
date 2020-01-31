ff1e81aa22fd905f47d641dec28ef697859f84d0
Dan McKenzie <mckenziejdan@gmail.com>
2020-01-31 08:52:46 +0000

    Create simple async component, to vue3 suspense api

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,29 @@
   <h1>Hello Vue 3!</h1>
   <button @click="inc">Clicked {{ count }} times.</button>
   <table>
-      <tr>
-        <fragment-component />
-      </tr>
-    </table>
+    <tr>
+      <fragment-component />
+    </tr>
+  </table>
+  <Suspense>
+    <template #default>
+      <async-component />
+    </template>
+    <template #fallback>
+      <div>Loading...</div>
+    </template>
+  </Suspense>
 </template>
 
 <script>
 import { ref } from 'vue'
 import FragmentComponent from './FragmentComponent.vue'
+import AsyncComponent from './AsyncComponent.vue';
 
 export default {
   components: {
     FragmentComponent,
+    AsyncComponent,
   },
   setup() {
     const count = ref(0)
---
+++ src/AsyncComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    This is our asynchronous component!!
+  </div>
+</template>
+
+<script lang="ts">
+
+async function wait(ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
+}
+export default {
+  async setup () {
+    await wait(3000)
+    return
+  }
+}
+</script>
